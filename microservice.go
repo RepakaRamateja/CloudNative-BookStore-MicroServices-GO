795e48d69c7ad978042d9eaeddf83b98c1eca36c
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2017-02-13 14:43:21 +0100

    Bug fixes.

--- microservice.go
+++ microservice.go
@@ -29,5 +29,5 @@ func port() string {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Welcome to Cloud Native Go.")
+	fmt.Fprintf(w, "Welcome to Cloud Native Go (Update).")
 }
